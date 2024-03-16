2d5baec01ec65332f9ef0a1fe66db1f7efe625c2
Pierre B <152717609+PTac-h@users.noreply.github.com>
2024-03-16 19:43:27 +0100

    g

--- Assets/Script/Rotor.cs
+++ Assets/Script/Rotor.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem;
 
 public class Rotor : MonoBehaviour
 {
@@ -7,7 +7,7 @@ public class Rotor : MonoBehaviour
     [SerializeField] int subdivision;
     [SerializeField] float bladeLength;
     [SerializeField] float bladeWidth;
-    [SerializeField] float bladeThickness; 
+    [SerializeField] float bladeThickness;
     [SerializeField] float bladeDensity = 2000f; // kg/m^3
     [SerializeField] float rpm;
 
@@ -20,6 +20,9 @@ public class Rotor : MonoBehaviour
     float bladeAngle;
     float spacing;
 
+    public float pitch = 0f;
+    public float roll = 0f;
+
     void Start()
     {
         angleBetweenBlades = 360 / blades;
@@ -29,6 +32,8 @@ public class Rotor : MonoBehaviour
 
     void FixedUpdate()
     {
+        DrawCardinal();
+
         bladeAngle = 0f;
 
         Vector3 originalVector = rotorShaft.forward;
@@ -37,17 +42,18 @@ public class Rotor : MonoBehaviour
         for (int j = 0; j < blades; j++)
         {
             bladeAngle += angleBetweenBlades; // Rotate by 90 degrees
-            
+
             // Create a rotation quaternion
+
             Quaternion rotation = Quaternion.AngleAxis(bladeAngle, axisOfRotation);
             Vector3 directionOfBlade = rotation * originalVector;
-            Debug.DrawRay(rotorShaft.position, directionOfBlade * bladeLength, Color.green);
+            Debug.DrawRay(rotorShaft.position, directionOfBlade * bladeLength, Color.white);
 
             for (int i = 0; i < subdivision; i++)
             {
                 Vector3 pointPosition = rotorShaft.position + directionOfBlade * spacing * i;
                 float distanceFromCenter = Vector3.Distance(pointPosition, rotorShaft.position);
-                Debug.DrawRay(pointPosition, rotorShaft.up * GetLift(distanceFromCenter) * Time.deltaTime, Color.blue) ;
+                Debug.DrawRay(pointPosition, rotorShaft.up * GetLift(distanceFromCenter) * Time.deltaTime, Color.magenta);
             }
         }
     }
@@ -56,7 +62,7 @@ public class Rotor : MonoBehaviour
     {
         float area = (bladeLength / subdivision) * bladeWidth;
         float volume = bladeLength * bladeWidth * bladeThickness;
-        float mass = GetWeight(volume,bladeDensity);
+        float mass = GetWeight(volume, bladeDensity);
 
         float angularVelocity = (2 * Mathf.PI * (rpm / 60)) * distanceFromCenter;
         float lift = liftCoefficient * airDensity * area * (angularVelocity * angularVelocity);
@@ -64,15 +70,42 @@ public class Rotor : MonoBehaviour
         return lift;
     }
 
-    void OnYaw()
-    {
 
-    }
-
-    public float GetWeight(float volume, float density)
+    private float GetWeight(float volume, float density)
     {
         float gravity = 9.81f;
         float weight = volume * density * gravity;
         return weight;
     }
+    private void DrawCardinal()
+    {
+        Quaternion rotationPitchPlus = Quaternion.AngleAxis(0f, rotorShaft.up);
+        Vector3 directionPitchPlus = rotationPitchPlus * rotorShaft.forward;
+        Debug.DrawRay(rotorShaft.position, directionPitchPlus * pitch, Color.green) ;
+
+        Quaternion rotationPitchMinus = Quaternion.AngleAxis(180f, rotorShaft.up);
+        Vector3 directionPitchMinus = rotationPitchMinus * rotorShaft.forward;
+        Debug.DrawRay(rotorShaft.position, directionPitchMinus * pitch, Color.yellow);
+
+        Quaternion rotationRollPlus = Quaternion.AngleAxis(90f, rotorShaft.up);
+        Vector3 directionRollPlus = rotationRollPlus * rotorShaft.forward;
+        Debug.DrawRay(rotorShaft.position, directionRollPlus * roll, Color.red);
+
+        Quaternion rotationRollMinus = Quaternion.AngleAxis(270f, rotorShaft.up);
+        Vector3 directionRollMinus = rotationRollMinus * rotorShaft.forward;
+        Debug.DrawRay(rotorShaft.position, directionRollMinus * roll, Color.blue);
+
+    }
+    #region input
+
+    public void OnPitch(InputAction.CallbackContext context)
+    {
+        pitch = context.ReadValue<float>();
+    }
+
+    public void OnRoll(InputAction.CallbackContext context)
+    {
+        roll = context.ReadValue<float>();
+    }
+    #endregion
 }
